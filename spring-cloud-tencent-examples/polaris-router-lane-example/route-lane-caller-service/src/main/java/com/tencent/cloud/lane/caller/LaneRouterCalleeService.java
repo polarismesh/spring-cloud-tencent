4f25daa83f4e68f7791e064888dce8bec8878e87
andrew shan <45474304+andrewshan@users.noreply.github.com>
2024-04-08 10:11:43 +0800

    fix: checkstyle issues

--- spring-cloud-tencent-examples/polaris-router-lane-example/route-lane-caller-service/src/main/java/com/tencent/cloud/lane/caller/LaneRouterCalleeService.java
+++ spring-cloud-tencent-examples/polaris-router-lane-example/route-lane-caller-service/src/main/java/com/tencent/cloud/lane/caller/LaneRouterCalleeService.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  *
  * @author Haotian Zhang
  */
-@FeignClient(value = "LaneCalleeService")
+@FeignClient("LaneCalleeService")
 public interface LaneRouterCalleeService {
 
 	/**
